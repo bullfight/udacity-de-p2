a13417a1508e87623c458e5009f6d034e94ea67e
Patrick Schmitz <patrick.schmitz@infogroup.com>
2020-03-06 15:57:23 -0800

    %s/datastore/data_store/

--- data_store.py
+++ data_store.py
@@ -27,32 +27,3 @@ class DataStore:
     def execute(self, sql, data=None):
         result = self.session.execute(sql, data)
         return result
-
-create_table_query = """
-    CREATE TABLE IF NOT EXISTS music_library
-    (
-        year        int,
-        artist_name text,
-        album_name  text,
-        city text,  PRIMARY KEY (year, artist_name, album_name)
-    )
-"""
-
-drop_table_query = """
-    DROP TABLE IF EXISTS music_library
-"""
-
-insert_query = """
-    INSERT INTO music_library (year, artist_name, album_name, city) VALUES (%s, %s, %s, %s)
-"""
-
-query = "SELECT * FROM music_library WHERE year = 1970"
-
-store = DataStore()
-store.execute(create_table_query)
-store.execute(insert_query, (1970, "The Beatles", "Let it Be", "Liverpool"))
-
-results = store.execute(query)
-print(results.one())
-
-store.execute(drop_table_query)
