161c16a5025f9dbb03fab4947e61e58501c7f55b
Patrick Schmitz <patrick.schmitz@infogroup.com>
2020-03-06 21:41:25 -0800

    Refactor Extract to use pandas

--- store/__init__.py
+++ store/__init__.py
@@ -1,3 +1,4 @@
+from .extract import Extract
 from .user_session import UserSession
 from .session import Session
 from .user_listen import UserListen
--- store/extract.py
+++ store/extract.py
@@ -1,55 +1,27 @@
 import os
 import glob
-import csv
+import pandas
 
 class Extract:
-    def __init__(self, filepath):
-        self.filepath = filepath
+    def __init__(self, directory):
+        self.directory = directory
 
-    def save(self, filename):
-        files = self.read_files()
-        data  = self.extract(files)
-        self.export(data, filename)
+    def save(self, filename, columns=None, dropna=None):
+        filepaths   = self.__get_filepaths()
+        data_frame  = self.__extract(filepaths)
+        self.__export(data_frame, filename, columns, dropna)
 
-    def read_files(self):
-        all_files = []
-        for root, dirs, files in os.walk(self.filepath):
-            files = glob.glob(os.path.join(root,'*.csv'))
-            for f in files :
-                all_files.append(os.path.abspath(f))
+    def __get_filepaths(self):
+        return glob.glob(os.path.join(self.directory, '*.csv'))
 
-        return all_files
+    def __extract(self, filepaths):
+        return pandas.concat(map(pandas.read_csv, filepaths))
 
-    def extract(self, file_path_list):
-        # initiating an empty list of rows that will be generated from each file
-        full_data_rows_list = []
+    def __export(self, data_frame, filename, columns, dropna):
+        if columns:
+            data_frame = data_frame[columns]
 
-        # for every filepath in the file path list
-        for f in file_path_list:
+        if dropna:
+            data_frame = data_frame.dropna(subset=dropna)
 
-            # reading csv file
-            with open(f, 'r', encoding = 'utf8', newline='') as csvfile:
-                # ceeating a csv reader object
-                csvreader = csv.reader(csvfile)
-                next(csvreader)
-
-                # extracting each data row one by one and append it
-                for line in csvreader:
-                    #print(line)
-                    full_data_rows_list.append(line)
-
-        return full_data_rows_list
-
-    def export(self, full_data_rows_list, filename):
-        # creating a smaller event data csv file called event_datafile_full csv that will be used to insert data into the
-        # Apache Cassandra tables
-        csv.register_dialect('myDialect', quoting=csv.QUOTE_ALL, skipinitialspace=True)
-
-        with open(filename, 'w', encoding = 'utf8', newline='') as f:
-            writer = csv.writer(f, dialect='myDialect')
-            writer.writerow(['artist','firstName','gender','itemInSession','lastName','length',\
-                    'level','location','sessionId','song','userId'])
-            for row in full_data_rows_list:
-                if (row[0] == ''):
-                    continue
-                writer.writerow((row[0], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[12], row[13], row[16]))
+        data_frame.to_csv(filename, index=False)
